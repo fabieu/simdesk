4933f2f59b3dcf6dff9d43c383f1d6fb83da66a9
Fabian Eulitz <dev@sustineo.de>
2022-10-09 01:35:48 +0200

    Initial commit

---
+++ src/main/java/de/sustineo/acc/leaderboards/LeaderboardsApplication.java
@@ -0,0 +1,13 @@
+package de.sustineo.acc.leaderboards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderboardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderboardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/sustineo/acc/leaderboards/LeaderboardsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sustineo.acc.leaderboards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderboardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
