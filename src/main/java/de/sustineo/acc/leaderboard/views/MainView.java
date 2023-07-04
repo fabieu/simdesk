a53f78d171a630d575455cf0126856141b1afc00
Fabian Eulitz <dev@sustineo.de>
2023-07-04 17:36:03 +0200

    Add notification when variable ENVIRONMENT=development is set

--- src/main/java/de/sustineo/acc/leaderboard/views/MainView.java
+++ src/main/java/de/sustineo/acc/leaderboard/views/MainView.java
@@ -1,11 +1,21 @@
 package de.sustineo.acc.leaderboard.views;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import de.sustineo.acc.leaderboard.configuration.PropertyConfiguration;
 import de.sustineo.acc.leaderboard.configuration.VaadinConfiguration;
 import de.sustineo.acc.leaderboard.layouts.MainLayout;
 
@@ -28,6 +38,33 @@ public class MainView extends VerticalLayout {
 
         layout.addAndExpand(new H1("Welcome to ACC Leaderboard"));
 
+        if ("development".equals(System.getenv(PropertyConfiguration.PROPERTY_ENVIRONMENT))){
+            createUnderDevelopmentNotification();
+        }
+
+
         return layout;
+    }
+
+    private void createUnderDevelopmentNotification() {
+        Notification notification = new Notification();
+        notification.addThemeVariants(NotificationVariant.LUMO_WARNING);
+        notification.setPosition(Notification.Position.BOTTOM_END);
+
+        Icon icon = VaadinIcon.WARNING.create();
+        Div info = new Div(new Text(VaadinConfiguration.APPLICATION_NAME + " is currently under development and not yet finished."));
+
+        Button closeButton = new Button(VaadinIcon.CLOSE.create());
+        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+        closeButton.getElement().setAttribute("aria-label", "Close");
+        closeButton.addClickListener(event -> {
+            notification.close();
+        });
+
+        HorizontalLayout layout = new HorizontalLayout(icon, info, closeButton);
+        layout.setAlignItems(Alignment.CENTER);
+
+        notification.add(layout);
+        notification.open();
     }
 }
