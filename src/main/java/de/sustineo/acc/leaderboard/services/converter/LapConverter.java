f0fb918c57ad92998dee77c89f8c6717ddb66d88
Fabian Eulitz <dev@sustineo.de>
2023-07-10 16:30:37 +0200

    Do not close the application because of session file issues

--- src/main/java/de/sustineo/acc/leaderboard/services/converter/LapConverter.java
+++ src/main/java/de/sustineo/acc/leaderboard/services/converter/LapConverter.java
@@ -6,12 +6,15 @@ import de.sustineo.acc.leaderboard.entities.json.AccCar;
 import de.sustineo.acc.leaderboard.entities.json.AccDriver;
 import de.sustineo.acc.leaderboard.entities.json.AccLap;
 import de.sustineo.acc.leaderboard.entities.json.AccSession;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Log
 @Service
 public class LapConverter extends BaseConverter {
     private final DriverConverter driverConverter;
@@ -23,18 +26,21 @@ public class LapConverter extends BaseConverter {
     public List<Lap> convertToLaps(Integer sessionId, AccSession accSession, FileMetadata fileMetadata) {
         return accSession.getLaps().stream()
                 .map(accLap -> convertToLap(sessionId, accLap, accSession, fileMetadata))
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
     }
 
     private Lap convertToLap(Integer sessionId, AccLap accLap, AccSession accSession, FileMetadata fileMetadata) {
         Optional<AccCar> accCar = accSession.getCarById(accLap.getCarId());
         if (accCar.isEmpty()) {
-            throw new RuntimeException(String.format("Car not found with id %s", accLap.getCarId()));
+            log.severe(String.format("Car not found with id %s", accLap.getCarId()));
+            return null;
         }
 
         Optional<AccDriver> accDriver = accCar.orElseThrow().getDriverByIndex(accLap.getDriverIndex());
         if (accDriver.isEmpty()) {
-            throw new RuntimeException(String.format("Driver not found with index %s", accLap.getDriverIndex()));
+            log.severe(String.format("Driver not found with index %s", accLap.getDriverIndex()));
+            return null;
         }
 
         return Lap.builder()
