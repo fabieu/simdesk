d4621306037b5ad0eafeb3b1515c6d2e858ab213
Fabian Eulitz <dev@sustineo.de>
2023-07-27 20:26:35 +0200

    Update references to GitLab project and default results folder

--- src/main/java/de/sustineo/acc/servertools/configuration/Reference.java
+++ src/main/java/de/sustineo/acc/servertools/configuration/Reference.java
@@ -2,5 +2,5 @@ package de.sustineo.acc.servertools.configuration;
 
 public class Reference {
     public static final String SUSTINEO = "https://sustineo.de";
-    public static final String PROJECT_WIKI = "https://gitlab.com/markracing/acc-leaderboard/-/wikis/home";
+    public static final String PROJECT_WIKI = "https://gitlab.com/markracing/acc-server-tools/-/wikis/home";
 }
