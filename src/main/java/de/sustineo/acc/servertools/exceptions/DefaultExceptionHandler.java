c3ce571eebc2eff633b2f1d1cab1620f7a347fa0
Fabian Eulitz <dev@sustineo.de>
2023-12-24 03:06:20 +0100

    Add default error views for all exceptions

---
+++ src/main/java/de/sustineo/acc/servertools/exceptions/DefaultExceptionHandler.java
@@ -0,0 +1,29 @@
+package de.sustineo.acc.servertools.exceptions;
+
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.ErrorParameter;
+import com.vaadin.flow.router.InternalServerError;
+import com.vaadin.flow.router.QueryParameters;
+import de.sustineo.acc.servertools.views.ErrorView;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.java.Log;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Log
+@Component
+public class DefaultExceptionHandler extends InternalServerError {
+    @Override
+    public int setErrorParameter(BeforeEnterEvent event, ErrorParameter<Exception> parameter) {
+        HashMap<String, List<String>> parameters = new HashMap<>();
+        parameters.put(ErrorView.QUERY_PARAMETER_HTTP_STATUS, List.of(HttpStatus.INTERNAL_SERVER_ERROR.toString()));
+        parameters.put(ErrorView.QUERY_PARAMETER_ERROR_MESSAGE, List.of(parameter.getException().getMessage()));
+
+        event.rerouteTo(ErrorView.class, new QueryParameters(parameters));
+
+        return HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+    }
+}
