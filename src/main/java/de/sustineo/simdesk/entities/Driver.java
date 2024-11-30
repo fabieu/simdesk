9f823c303d71984390bb30bf28589f159320f528
Fabian Eulitz <dev@sustineo.de>
2024-11-30 02:17:38 +0100

    Revert toString for Driver

--- src/main/java/de/sustineo/simdesk/entities/Driver.java
+++ src/main/java/de/sustineo/simdesk/entities/Driver.java
@@ -63,4 +63,9 @@ public class Driver extends Entity {
     public int hashCode() {
         return playerId.hashCode();
     }
+
+    @Override
+    public String toString() {
+        return getFullNameCensored();
+    }
 }
