2e548a7998dbdfb20316cf8c15513b48426aa70d
Fabian Eulitz <dev@sustineo.de>
2025-01-24 00:20:05 +0100

    Update user and user permission tables
    - Reserver first 100 userIds for system users

--- src/main/resources/db/migration/postgres/V2_4_0__api-keys.sql
+++ src/main/resources/db/migration/postgres/V2_4_0__api-keys.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS user_permission;
-DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS simdesk.user_permission;
+DROP TABLE IF EXISTS simdesk.user;
 
-CREATE TABLE "user"
+CREATE TABLE simdesk.user
 (
-    user_id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id         SERIAL PRIMARY KEY,
     username        TEXT      NOT NULL UNIQUE,
     password        TEXT,
     type            TEXT      NOT NULL,
@@ -15,27 +15,25 @@ CREATE TABLE user_permission
     user_id INTEGER,
     role    TEXT,
     PRIMARY KEY (user_id, role),
-    FOREIGN KEY (user_id) REFERENCES "user" (user_id),
-    FOREIGN KEY (role) REFERENCES user_role (name)
+    FOREIGN KEY (user_id) REFERENCES simdesk.user (user_id),
+    FOREIGN KEY (role) REFERENCES simdesk.user_role (name)
 );
 
 CREATE TABLE IF NOT EXISTS user_api_key
 (
-    api_key_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    api_key_id        SERIAL PRIMARY KEY,
     user_id           INTEGER   NOT NULL,
     api_key           TEXT      NOT NULL UNIQUE,
     name              TEXT      NOT NULL,
     active            BOOLEAN   NOT NULL DEFAULT TRUE,
     creation_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES "user" (user_id)
+    FOREIGN KEY (user_id) REFERENCES simdesk.user (user_id)
 );
 
-INSERT INTO "user" (type, user_id, username)
+INSERT INTO simdesk.user (type, user_id, username)
 VALUES ('SYSTEM', 1, 'admin');
 
-INSERT INTO user_permission (user_id, role)
+INSERT INTO simdesk.user_permission (user_id, role)
 VALUES (1, 'ROLE_ADMIN');
 
-UPDATE sqlite_sequence
-SET seq=99
-WHERE name = 'user';
+ALTER SEQUENCE simdesk.user_user_id_seq restart with 100;
